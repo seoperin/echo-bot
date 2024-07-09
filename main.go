d25e4d28d4caa22170da81140fd0788c85509bd8
seoperin <seoperin@gmail.com>
2024-07-09 14:43:30 +0800

    messages count

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
-	"strconv"
+	"sync"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
+var (
+	messageCounts map[int64]int
+	mutex         sync.Mutex
+)
+
+func init() {
+	messageCounts = make(map[int64]int)
+}
+
+func incrementMessageCount(userID int64) int {
+	mutex.Lock()
+	defer mutex.Unlock()
+	messageCounts[userID]++
+	return messageCounts[userID]
+}
 
 func main() {
 	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
@@ -26,14 +43,20 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	//messageCounts := make(map[string]int)
-
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message != nil { // If we got a message
 			userID := update.Message.From.ID
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Ты "+strconv.FormatInt(userID, 36)+" написал: "+update.Message.Text)
+			userMessage := update.Message.Text
+
+			// Increment the message count for the user
+			count := incrementMessageCount(userID)
+
+			// Prepare the response message
+			responseText := fmt.Sprintf("Ты написал: %s\nВсего сообщений: %d", userMessage, count)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, responseText)
 			bot.Send(msg)
 		}
 	}
